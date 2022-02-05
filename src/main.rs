09d9d6b3bae23b942d699ac60a81baacd669c555
Jeff Wang <jeffawang@gmail.com>
2022-02-04 16:51:27 -0800

    move mod instancing to instancing.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // mod pipeline;
+mod instancing;
 mod texture;
 
 use bytemuck::{self};
@@ -18,111 +19,6 @@ const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
     0.0 * NUM_INSTANCES_PER_ROW as f32 * 0.5,
     // NUM_INSTANCES_PER_ROW as f32 * 0.5,
 );
-
-mod instancing {
-    use cgmath::prelude::*;
-
-    use wgpu::util::DeviceExt;
-
-    use super::INSTANCE_DISPLACEMENT;
-
-    use super::NUM_INSTANCES_PER_ROW;
-
-    pub(crate) struct Instance {
-        pub(crate) position: cgmath::Vector3<f32>,
-        pub(crate) rotation: cgmath::Quaternion<f32>,
-    }
-
-    impl Instance {
-        pub(crate) fn to_raw(&self) -> InstanceRaw {
-            InstanceRaw {
-                model: (cgmath::Matrix4::from_translation(self.position)
-                    * cgmath::Matrix4::from(self.rotation))
-                .into(),
-            }
-        }
-    }
-
-    pub(crate) fn instancing(device: &wgpu::Device) -> (Vec<Instance>, wgpu::Buffer) {
-        let instances = (0..NUM_INSTANCES_PER_ROW)
-            .flat_map(|_z| {
-                (0..NUM_INSTANCES_PER_ROW).map(move |_x| {
-                    let position = cgmath::Vector3 {
-                        x: 0.0,
-                        // x: x as f32,
-                        y: 0.0,
-                        z: 0.0,
-                    } - INSTANCE_DISPLACEMENT;
-
-                    let rotation = if position.is_zero() {
-                        // this is needed so an object at (0,0,0) won't get scaled to zero
-                        // as Quaternions can affect scale if they're not created correctly
-                        cgmath::Quaternion::from_axis_angle(
-                            cgmath::Vector3::unit_z(),
-                            cgmath::Deg(0.0),
-                        )
-                    } else {
-                        cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(45.0))
-                    };
-
-                    Instance { position, rotation }
-                })
-            })
-            .collect::<Vec<_>>();
-        let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
-        let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-            label: Some("Instance buffer"),
-            contents: bytemuck::cast_slice(&instance_data),
-            usage: wgpu::BufferUsages::VERTEX,
-        });
-        (instances, instance_buffer)
-    }
-
-    #[repr(C)]
-    #[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
-    pub(crate) struct InstanceRaw {
-        pub(crate) model: [[f32; 4]; 4],
-    }
-
-    impl InstanceRaw {
-        pub(crate) fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
-            use std::mem;
-            wgpu::VertexBufferLayout {
-                array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
-                // We still need to switch from using a step mode of Vertex to Instance
-                // This means that our shaders wil only change to use the next
-                // instance when the shader starts processing a new instance.
-                step_mode: wgpu::VertexStepMode::Instance,
-                attributes: &[
-                    wgpu::VertexAttribute {
-                        offset: 0,
-                        // While our vertex shader only use locations 0 and 1 now,
-                        // we will use 2,3, and 4 in later tutorials.
-                        shader_location: 5,
-                        format: wgpu::VertexFormat::Float32x4,
-                    },
-                    // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
-                    // for each vec4 and reassemble the mat4 in the shader.
-                    wgpu::VertexAttribute {
-                        offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
-                        shader_location: 6,
-                        format: wgpu::VertexFormat::Float32x4,
-                    },
-                    wgpu::VertexAttribute {
-                        offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
-                        shader_location: 7,
-                        format: wgpu::VertexFormat::Float32x4,
-                    },
-                    wgpu::VertexAttribute {
-                        offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
-                        shader_location: 8,
-                        format: wgpu::VertexFormat::Float32x4,
-                    },
-                ],
-            }
-        }
-    }
-}
 
 mod vertex {
     #[repr(C)]
@@ -519,7 +415,8 @@ impl State {
 
         let camera = camera::new_camera(&config, &device);
 
-        let (instances, instance_buffer) = instancing::instancing(&device);
+        let (instances, instance_buffer) =
+            instancing::new(&device, NUM_INSTANCES_PER_ROW, INSTANCE_DISPLACEMENT);
 
         let depth_texture =
             texture::Texture::create_depth_texture(&device, &config, "depth_texture");
